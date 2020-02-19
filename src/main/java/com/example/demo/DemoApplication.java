5fce192355a7204e21d9626ebc1b25b44a389d0b
Nikhila <nikhiladamaraju30@gmail.com>
2020-02-19 15:39:48 -0800

    g

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootservletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+	@Override
+	protected springApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CognizantApplication.class);
+	}
 }
